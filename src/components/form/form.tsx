2d05aad4466fda2675ae280a755717ed9ad9fcd3
Georg <georgiy.nikitin@gmail.com>
2020-08-30 12:23:35 +0300

    Добавляет стили

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -27,16 +27,20 @@ const Main: React.FC<Props> = (props: Props) => {
               onSubmit={handleSubmitForm}
         >
           <div className="organization__text">
-            <label className="organization__label" htmlFor="organization" >Organization</label>
+            {/*<label className="organization__label" htmlFor="organization" >Organization</label>*/}
             <input className="organization__input"
-                  onChange={handleInputChange}
-                  value={value}
-                  name="organization"
-                  id="organization"
-                  placeholder="Name organization"
-                  required
+                   onChange={handleInputChange}
+                   value={value}
+                   name="organization"
+                   type="text"
+                   aria-label="Find repositories"
+                   id="organization"
+                   placeholder="Find repositories"
+                   required
             />
-            <button className="btn organization__btn" type="submit" >Post</button>
+            <button className="organization__btn" type="submit" tabIndex={0} >
+              <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17"><path fill="#D21EB4" d="M17 15.586l-3.542-3.542A7.465 7.465 0 0 0 15 7.5 7.5 7.5 0 1 0 7.5 15c1.71 0 3.282-.579 4.544-1.542L15.586 17 17 15.586zM2 7.5C2 4.467 4.467 2 7.5 2 10.532 2 13 4.467 13 7.5c0 3.032-2.468 5.5-5.5 5.5A5.506 5.506 0 0 1 2 7.5z"/></svg>
+            </button>
           </div>
 
         </form>
